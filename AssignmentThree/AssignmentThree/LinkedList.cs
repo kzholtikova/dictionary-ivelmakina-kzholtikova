41d129333653e2084279690259310ead620cd62a
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-02-21 16:56:08 +0200

    overriden ToString methods
    - to print pair directly to the console

--- AssignmentThree/AssignmentThree/LinkedList.cs
+++ AssignmentThree/AssignmentThree/LinkedList.cs
@@ -56,16 +56,40 @@ public class LinkedList
         
         return currentNode;
     }
+
+    public override string ToString()
+    {
+        string result = "";
+        LinkedListNode? currentNode = _head;
+        
+        while (currentNode != null)
+        {
+            result += currentNode.ToString();
+            currentNode = currentNode.Next;
+        }
+
+        return result;
+    }
 }
 
 public class LinkedListNode(KeyValuePair pair, LinkedListNode? next = null)
 {
     public KeyValuePair Pair { get; } = pair;
     public LinkedListNode? Next { get; set; } = next;
+
+    public override string ToString()
+    {
+        return $"{Pair}\n";
+    }
 }
 
 public class KeyValuePair(string key, string value)
 {
     public string Key { get; } = key;
     public string Value { get;  } = value;
+
+    public override string ToString()
+    {
+        return $"{Key}: {Value}";
+    }
 }
