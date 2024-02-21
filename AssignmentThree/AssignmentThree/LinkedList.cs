4b3c0713a126c0d78f007b491101b2ee046c3c19
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-02-21 16:08:58 +0200

    linked list Add()

--- AssignmentThree/AssignmentThree/LinkedList.cs
+++ AssignmentThree/AssignmentThree/LinkedList.cs
@@ -1,5 +1,25 @@
 namespace AssignmentThree;
 
+public class LinkedList
+{
+    private LinkedListNode? _head;
+
+    public void Add(KeyValuePair pair)
+    {
+        if (_head is not null)
+        {
+            LinkedListNode currentNode = _head;
+            
+            while (currentNode.Next is not null)
+            {
+                currentNode = currentNode.Next;
+            }
+
+            currentNode.Next = new LinkedListNode(pair);
+        } else { _head = new LinkedListNode(pair); }
+    }
+}
+
 public class LinkedListNode(KeyValuePair pair, LinkedListNode? next = null)
 {
     public KeyValuePair Pair { get; } = pair;
