252edc271b18f83b1b3e0095bf0c723e115af334
Kira Zholtikova <kzholtikova@kse.org.ua>
2024-02-21 16:06:17 +0200

    linked list node class
    - based on key-value pair
    - pointer on the next

--- AssignmentThree/AssignmentThree/LinkedList.cs
+++ AssignmentThree/AssignmentThree/LinkedList.cs
@@ -1,5 +1,11 @@
 namespace AssignmentThree;
 
+public class LinkedListNode(KeyValuePair pair, LinkedListNode? next = null)
+{
+    public KeyValuePair Pair { get; } = pair;
+    public LinkedListNode? Next { get; set; } = next;
+}
+
 public class KeyValuePair(string key, string value)
 {
     public string Key { get; } = key;
