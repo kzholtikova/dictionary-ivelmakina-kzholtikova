0ed6ee26f00027b5432634a4c0613ea72309c8c1
Iryna Velmakina <150923968+iravelmakina@users.noreply.github.com>
2024-02-25 22:40:48 +0200

    initialize main program structure
    - set up the main program structure with file path and existence check
    - added methods for loading a dictionary from a file and running interactive mode
    - included error handling for dictionary loading failure

--- AssignmentThree/AssignmentThree/Program.cs
+++ AssignmentThree/AssignmentThree/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     public static void Main()
     {
-    
+        string filePath = "C:/run" +
+                          "Users/user/Documents/GitHub/assignment-three-v3-ivelmakina-kzholtikova/" +
+                          "AssignmentThree/AssignmentThree/Dictionary.txt"; // change filepath
+
+        if (File.Exists(filePath))
+        {
+            StringsDictionary? dictionary = LoadDictionaryFromFile(filePath);
+
+            if (dictionary != null)
+            {
+                RunDictionaryInteractiveMode(dictionary);
+            }
+            else
+            {
+                Console.WriteLine("Failed to load the dictionary. Exiting...");
+            }
+        }
+        else
+        {
+            Console.WriteLine("File was not found");
+        }
+    }
+
+    private static StringsDictionary? LoadDictionaryFromFile(string filePath)
+    {
+        
+    }
+
+    private static void RunDictionaryInteractiveMode(StringsDictionary? dictionary)
+    {
+        
     }
 }
